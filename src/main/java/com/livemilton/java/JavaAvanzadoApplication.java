2488f56215a68730ce79fbfd2d5d00bd144fd689
Milton Munoz <livemilton@gmail.com>
2025-08-04 05:45:25 -0500

    first commit

---
+++ src/main/java/com/livemilton/java/JavaAvanzadoApplication.java
@@ -0,0 +1,13 @@
+package com.livemilton.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAvanzadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAvanzadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livemilton/java/JavaAvanzadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livemilton.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAvanzadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
