222dbe789ec2865ea7dc69441c1d6ae905a1326b
Milton Munoz <livemilton@gmail.com>
2025-08-17 20:37:07 -0500

    Project-Streams :sparkles: repository

---
+++ src/main/java/com/livemilton/java/projectstreams/product/interfaces/ProductRepository.java
@@ -0,0 +1,19 @@
+package com.livemilton.java.projectstreams.product.interfaces;
+
+import com.livemilton.java.projectstreams.product.exceptions.InvalidProductException;
+import com.livemilton.java.projectstreams.product.model.Product;
+import com.livemilton.java.projectstreams.product.model.ProductCategory;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductRepository {
+
+    List<Product> findAll() throws InvalidProductException;
+    Optional<Product> findById(Long id);
+    void save(Product product);
+    void delete(Long id);
+    void update(Optional<Product> product);
+    boolean existByid(Long id);
+
+}
---
+++ src/main/java/com/livemilton/java/projectstreams/product/repository/ProductRepositoryServices.java
@@ -0,0 +1,49 @@
+package com.livemilton.java.projectstreams.product.repository;
+
+import com.livemilton.java.projectstreams.product.exceptions.InvalidProductException;
+import com.livemilton.java.projectstreams.product.interfaces.ProductRepository;
+import com.livemilton.java.projectstreams.product.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ProductRepositoryServices implements ProductRepository {
+
+    private final List<Product> products = new ArrayList<>();
+
+    @Override
+    public List<Product> findAll() throws InvalidProductException {
+        if(products.isEmpty()){
+            throw new InvalidProductException("La lista esta vacia");
+        }
+        return products;
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return products.stream()
+                .filter(product -> product.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public void save(Product product) {
+        products.add(product);
+    }
+
+    @Override
+    public void delete(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    @Override
+    public void update(Optional<Product> product) {
+
+    }
+
+    @Override
+    public boolean existByid(Long id) {
+        return products.stream().anyMatch(product -> product.getId().equals(id));
+    }
+}
