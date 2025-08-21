69b2677384dddbc9b7e73c1d003bb1a60699c587
Milton Munoz <livemilton@gmail.com>
2025-08-20 22:25:58 -0500

    Thread-concurrence :sparkles: Callable & Future

---
+++ src/main/java/com/livemilton/java/threads/executor/SumCalculator.java
@@ -0,0 +1,51 @@
+package com.livemilton.java.threads.executor;
+
+import java.util.concurrent.*;
+
+public class SumCalculator implements Callable<Integer> {
+
+    private int number1;
+    private int number2;
+
+    public SumCalculator(int number1, int number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Tarea:  Iniciando suma...");
+
+        Thread.sleep(1500);
+
+        int sum = number1 + number2;
+
+        System.out.println("Name: " + Thread.currentThread().getName());
+
+        System.out.println("Tarea: Suma completada");
+        return sum;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        Callable<Integer> sumTask = new SumCalculator(5,60);
+
+        //Future. to obtain results for a taks thats execute in another thread
+        Future<Integer> result = executorService.submit(sumTask);
+
+        while(!result.isDone()){
+            System.out.println("Procesando.....");
+            try{
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        System.out.println("Resultado = " + result.get());
+
+        executorService.shutdown();
+    }
+
+
+}
