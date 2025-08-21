a158fde671f801959e13c1bfb022a0543f984735
Milton Munoz <livemilton@gmail.com>
2025-08-20 21:13:06 -0500

    Thread-concurrence :sparkles: concurrence vs paralelism

---
+++ src/main/java/com/livemilton/java/threads/executor/ConcurrenceExample.java
@@ -0,0 +1,43 @@
+package com.livemilton.java.threads.executor;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ConcurrenceExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.out.println("Ejecutando newSingleThreadExecutor");
+        ExecutorService single = Executors.newSingleThreadExecutor();
+
+        long start = System.currentTimeMillis();
+        //submit method in a executor sends a taks that have been of runnable type for execute in a thread of the pool.
+        single.submit(()-> task("Tarea A"));
+        single.submit(()-> task("Tarea B"));
+        single.submit(()-> task("Tarea C"));
+
+        single.shutdown();
+
+        if(!single.awaitTermination(1, TimeUnit.MINUTES)){
+            System.out.println("Tareas demoradas, forzando salida");
+            single.shutdown();
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Tiempo total: " + (end-start)+ " ms");
+    }
+
+    public static void task(String name){
+        System.out.println("Iniciando la tarea " + Thread.currentThread().getName());
+
+        try{
+            Thread.sleep(1500);
+        }catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Tarea completada en hilo " + Thread.currentThread().getName());
+    }
+}
