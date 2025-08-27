43cba80c4d24e47a200fdd842417bd736e268daf
Milton Munoz <livemilton@gmail.com>
2025-08-26 22:32:31 -0500

    Thread-concurrence :sparkles: project model

--- src/main/java/com/livemilton/java/project/concurrence/log/model/LogSummary.java
+++ src/main/java/com/livemilton/java/project/concurrence/log/model/LogSummary.java
@@ -1,4 +1,21 @@
 package com.livemilton.java.project.concurrence.log.model;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.util.Map;
+import java.util.Set;
+
+@AllArgsConstructor
+@ToString
 public class LogSummary {
+
+    private int totalEntries;
+    private int errorCount;
+    private Set<String> uniqueUsers;
+    private double averageResponseTime;
+    private Map<Integer, Long> errorCountsByCode;
+
+
+
 }
